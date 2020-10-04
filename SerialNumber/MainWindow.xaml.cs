91ce74d8dff9298acdfea79855fdefde0e801805
Kuda-ob84 <65267697+Kuda-ob84@users.noreply.github.com>
2020-10-04 17:45:01 +0600

    Добавьте файлы проекта.

---
+++ SerialNumber/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SerialNumber
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+ 
+
+        private void bSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var sr = new StreamReader("data\\" + tbLogin.Text + "\\data.txt");
+                string encusr = sr.ReadLine();
+                string encpass = sr.ReadLine();
+                string serialNumber = sr.ReadLine();
+                sr.Close();
+
+                string decusr = CryptData.Decrypt(encusr);
+                string decpass = CryptData.Decrypt(encpass);
+
+                if (decusr == tbLogin.Text && decpass == tbPassword.Password && serialNumber == CryptData.getSN())
+                {
+                    MessageBox.Show("Добро пожаловать", decusr);
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Пароль или логин не правильны.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Пароль или логин не правильны.");
+            }
+        }
+
+        private void bSighUp_Click(object sender, RoutedEventArgs e)
+        {
+            Signup signUp = new Signup();
+            signUp.Show();
+        }
+
+  
+    }
+}
---
+++ SerialNumber/Signup.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SerialNumber
+{
+    /// <summary>
+    /// Логика взаимодействия для Signup.xaml
+    /// </summary>
+    public partial class Signup : Window
+    {
+        //string keyFileName = @"C:\Users\Dauken\source\repos\SerialNumberUse\SerialNumberUse\symmetric_key.config";
+
+        public Signup()
+        {
+            InitializeComponent();
+            tbSerialNumber.Text = CryptData.getSN();
+        }
+
+        private void bSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            WriteData();
+        }
+
+
+        public String doHash(byte[] val)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(val);
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+        private void tbLogin_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void tbPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (tbPassword.Text == String.Empty)
+            {
+                tbHash.Text = String.Empty;
+            }
+            else
+            {
+                byte[] passtohash = System.Text.Encoding.UTF8.GetBytes(tbPassword.Text.ToString());
+                tbHash.Text = doHash(passtohash);
+            }
+        }
+
+        public void WriteData()
+        {
+            if (tbLogin.Text.Length < 6 || tbPassword.Text.Length < 7)
+            {
+                MessageBox.Show("Username or password is too short");
+            }
+            else
+            {
+                string dir = tbLogin.Text;
+                Directory.CreateDirectory("data\\" + dir);
+
+                var sw = new StreamWriter("data\\" + dir + "\\data.txt");
+
+                string encusr = CryptData.Encrypt(tbLogin.Text);
+
+                string encpass = CryptData.Encrypt(tbPassword.Text);
+
+                sw.WriteLine(encusr);
+                sw.WriteLine(encpass);
+                sw.WriteLine(CryptData.getSN());
+                sw.Close();
+
+                MessageBox.Show("User was succesfully created", tbLogin.Text);
+                this.Close();
+            }
+
+        }
+    }
+}
